086e3bc7bde4661424f17afcc336982b5de0e08a
nagaohiroki <testplayers0226@gmail.com>
2019-04-19 08:13:10 +0900

    update matrix

--- src/matrix.py
+++ src/matrix.py
@@ -2,21 +2,19 @@ import numpy as np
 import activation_function
 
 
+def layer(X, W1, B1):
+    A1 = np.dot(X, W1) + B1
+    return activation_function.sigmoid(A1)
+
+
 def matrix_test():
     X = np.array([1.0, 0.5])
-    # layer 1
     W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
     B1 = np.array([0.1, 0.2, 0.3])
-    A1 = np.dot(X, W1) + B1
-    Z1 = activation_function.sigmoid(A1)
-    print(A1)
-    print(Z1)
-    # layer2
     W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
     B2 = np.array([0.1, 0.2])
-    A2 = np.dot(Z1, W2) + B2
-    Z2 = activation_function.sigmoid(A2)
-    print(A2)
+    Z1 = layer(X, W1, B1)
+    Z2 = layer(Z1, W2, B2)
     print(Z2)
 
 
