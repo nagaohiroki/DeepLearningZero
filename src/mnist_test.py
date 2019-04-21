bbd318606b7134fbdaddbcb773fa5ccfe7a3295e
nagaohiroki <testplayers0226@gmail.com>
2019-04-21 23:41:44 +0900

    add mnist_test

--- src/mnist_test.py
+++ src/mnist_test.py
@@ -1,5 +1,7 @@
 import dataset.mnist
+import pickle
 import numpy as np
+import activation_function
 from PIL import Image
 
 
@@ -8,7 +10,7 @@ def img_show(img):
     pil_img.show()
 
 
-def main():
+def mnist_test():
     data = dataset.mnist.load_mnist(flatten=True, normalize=False)
     (x_train, t_train), (x_test, t_test) = data
     print(x_train.shape)
@@ -21,6 +23,40 @@ def main():
     print(img.shape)
     new_img = img.reshape(28, 28)
     img_show(new_img)
+
+
+def init_network():
+    with open('dataset/mnist.pkl', 'rb') as f:
+        network = pickle.load(f)
+    return network
+
+
+def get_data():
+    data = dataset.mnist.load_mnist(flatten=True, normalize=False)
+    (x_train, t_train), (x_test, t_test) = data
+    return x_test, t_test
+
+
+def predict(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+    a1 = np.dot(x, W1) + b1
+    z1 = activation_function.sigmoid(a1)
+    a2 = np.dot(z1, W2) + b2
+    z2 = activation_function.sigmoid(a2)
+    a3 = np.dot(z2, W3) + b3
+    return activation_function.softmax(a3)
+
+
+def main():
+    pass
+    '''
+    x, t = get_data()
+    network = init_network()
+    accuracy_cnt = 0
+    for i in range(len(x)):
+        y = predict(network, x[i])
+    '''
 
 
 if __name__ == "__main__":
