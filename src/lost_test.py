bb1ccaa541ffda1be87392c44575dea7fc08b13b
nagaohiroki <testplayers0226@gmail.com>
2019-04-30 15:17:17 +0900

    update lost_test.py

--- src/lost_test.py
+++ src/lost_test.py
@@ -31,14 +31,18 @@ def mini_bacth_test():
     print('t_batch.shape', t_batch.shape)
 
 
-def main():
+def error_test():
     y = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
     t = np.array([0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0])
     print(y.shape)
     print(t.shape)
     print('mean_squared_error', mean_squared_error(y, t))
     print('cross_entropy_error', cross_entropy_error(y, t))
+
+
+def main():
     mini_bacth_test()
+    error_test()
 
 
 if __name__ == '__main__':
