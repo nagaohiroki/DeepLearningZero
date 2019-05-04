55962454ab94e0c10e8c630acfe9b87f4a58869c
nagaohiroki <testplayers0226@gmail.com>
2019-05-04 23:47:00 +0900

    add gradient function

--- src/lost_test.py
+++ src/lost_test.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pylab as plt
 import dataset.mnist
 
 
@@ -39,9 +40,33 @@ def error_test():
     print('cross_entropy_error', cross_entropy_error(y, t))
 
 
+def numerical_diff(f, x):
+    h = 10e-50
+    return (f(x + h) - f(x)) / h
+
+
+def function(x):
+    return np.sum(x ** 2)
+
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+    return grad
+
+
 def main():
-    mini_bacth_test()
-    error_test()
+    print(numerical_gradient(function, np.array([3.0, 4.0])))
+    print(numerical_gradient(function, np.array([0.0, 2.0])))
+    print(numerical_gradient(function, np.array([3.0, 0.0])))
 
 
 if __name__ == '__main__':
